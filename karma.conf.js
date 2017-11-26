37b19d8be9a58fde401b1570582bf83dcb432d90
Борис Медведев <borismedvedev@MBP-Boris.Dlink>
2017-11-26 20:22:27 +0300

    add server tests

--- karma.conf.js
+++ karma.conf.js
@@ -22,7 +22,7 @@ module.exports = function(config) {
         port: 9876,
         browsers: ['ChromeHeadless'], // или Chrome или Firefox
         captureTimeout: 20000,
-        singleRun: true,
+        singleRun: false,
         plugins: [
             require('karma-mocha'),
             require('karma-chai'),
